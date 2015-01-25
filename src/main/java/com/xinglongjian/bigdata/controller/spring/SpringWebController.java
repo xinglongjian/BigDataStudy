3ea5a3ed5c0e340d04953fdf703875714275ec1e
xinglongjian <xinglongjian@qq.com>
2015-01-25 13:11:11 +0800

    add Spring xml OR JSON Support

--- src/main/java/com/xinglongjian/bigdata/controller/spring/SpringWebController.java
+++ src/main/java/com/xinglongjian/bigdata/controller/spring/SpringWebController.java
@@ -1,14 +1,19 @@
 package com.xinglongjian.bigdata.controller.spring;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.xinglongjian.bigdata.web.model.Order;
 import com.xinglongjian.bigdata.web.site.FrontUtils;
 import com.xinglongjian.bigdata.web.site.SiteUtils;
 import com.xinglongjian.bigdata.web.site.StudySite;
@@ -61,4 +66,66 @@ public class SpringWebController {
 		return "excel";
 	}
 
+
+	/**
+	 * 
+	 * @param locale
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/spring/order", method = RequestMethod.GET)
+	public String order(Locale locale, ModelMap model,HttpServletRequest request) {
+		StudySite site=SiteUtils.getSiteFromRequest(request);
+		FrontUtils.frontData(request, model, site);
+		List<Order> orders=new ArrayList<Order>();
+		orders.add(new Order("1111", "1111", 10, new Date()));
+		orders.add(new Order("2222", "2222", 12, new Date()));
+		orders.add(new Order("3333", "3333", 13, new Date()));
+		orders.add(new Order("4444", "4444", 14, new Date()));
+		orders.add(new Order("5555", "5555", 15, new Date()));
+		model.addAttribute("orders",orders);
+		model.addAttribute("menu","spring");
+		model.addAttribute("ddmenu","springweb");
+		model.addAttribute("submenu","order");
+		return "spring/springweb/order";
+	}
+	
+	/**
+	 * for XML
+	 * @param locale
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/spring/order.xml", method = RequestMethod.GET)
+	public String xml(Locale locale, ModelMap model,HttpServletRequest request) {
+		List<Order> orders=new ArrayList<Order>();
+		orders.add(new Order("1111", "1111", 10, new Date()));
+		orders.add(new Order("2222", "2222", 12, new Date()));
+		orders.add(new Order("3333", "3333", 13, new Date()));
+		orders.add(new Order("4444", "4444", 14, new Date()));
+		orders.add(new Order("5555", "5555", 15, new Date()));
+		model.addAttribute("orders",orders);
+		return "order";
+	}
+	
+	/**
+	 * for JSON
+	 * @param locale
+	 * @param model
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/spring/order.json", method = RequestMethod.GET)
+	public String json(Locale locale, ModelMap model,HttpServletRequest request) {
+		List<Order> orders=new ArrayList<Order>();
+		orders.add(new Order("1111", "1111", 10, new Date()));
+		orders.add(new Order("2222", "2222", 12, new Date()));
+		orders.add(new Order("3333", "3333", 13, new Date()));
+		orders.add(new Order("4444", "4444", 14, new Date()));
+		orders.add(new Order("5555", "5555", 15, new Date()));
+		model.addAttribute("orders",orders);
+		return "order";
+	}
 }
