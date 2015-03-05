a51befc397865a29e98f0211c87d28ec80f03518
xinglongjian <xinglongjian@qq.com>
2015-03-05 21:16:59 +0800

    add spring hessian service

--- src/main/java/com/xinglongjian/remoting/caucho/server/SplitService.java
+++ src/main/java/com/xinglongjian/remoting/caucho/server/SplitService.java
@@ -1,9 +1,11 @@
 package com.xinglongjian.remoting.caucho.server;
 
-public class SplitService implements ISplitService {
+import org.springframework.remoting.caucho.HessianProxyFactoryBean;
 
+public class SplitService implements ISplitService {
 	@Override
 	public String getValues() {
+		HessianProxyFactoryBean d;
 		// TODO Auto-generated method stub
 		return "This is my Hessian service";
 	}
